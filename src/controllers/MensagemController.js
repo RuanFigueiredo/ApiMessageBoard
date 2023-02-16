4eeac83f3e932eaea817a34c328451a805ca61f4
RuanFigueiredo <ruanthiago67@gmail.com>
2023-02-15 23:20:53 -0300

    atualizando mensagem

--- src/controllers/MensagemController.js
+++ src/controllers/MensagemController.js
@@ -1,3 +1,4 @@
+const uuid = require('uuid')
 const banco = []
 
 class MensagemController{
@@ -6,12 +7,27 @@ class MensagemController{
     }
     adicionarMensagem(req,res){
         const mensagem = {
+            id:uuid.v4(),
             autor:req.body.autor,
             mensagem:req.body.mensagem
         }
         banco.push(mensagem)
         res.status(201)
         res.json(mensagem)
+    }
+    atualizarMensagem(req,res){
+        const id = req.params.id
+        const encontrador = function(mensagemBanco){
+            return mensagemBanco.id == id
+        }
+        const atualizarMensagem = banco.find(encontrador)
+        if(atualizarMensagem == null){
+            res.status(404)
+            res.end()
+        }else{
+            atualizarMensagem.mensagem = req.body.mensagem
+            res.json(atualizarMensagem)
+        }
     }
 }
 
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -6,6 +6,8 @@ router.get('/mensagem', MensagemController.listarMensagens)
 
 router.post('/mensagem', MensagemController.adicionarMensagem)
 
+router.put('/mensagem/:id', MensagemController.atualizarMensagem)
+
 router.get('/', (req, res) => {
     res.send('Hello World!')
   })
